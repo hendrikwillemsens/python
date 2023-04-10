8046a29fb4fcc0e053b9d62db7e889ed766c9189
hendrikwillemsens <hendrik.willemsens@hotmail.com>
2023-04-10 15:56:28 +0200

    20230410

---
+++ chapter4/Untitled-1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def length(row):
+
+    print("de lengthe of deze lijst is {}".format(len(row)))
+
+row = [19, 19, 15, 5, 5, 5, 1, 2]
+
+length(row)
+
+print("het vijfde element is {}".format(row[5]))
--- symbolen.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-Day = int(input("Day of birth"))
-Month = input("month of birth")
-
-if(Day in range(19, 23, 1)) AND (Month):
-   
-   print("Y")
