c5768712a04a8a921429686d50cc806c5a72a064
hendrikwillemsens <hendrik.willemsens@hotmail.com>
2023-11-10 09:01:23 +0100

    d

--- chapter5/ttt.py
+++ chapter5/ttt.py
@@ -1,5 +1,7 @@
-import tkinter as tk
+import streamlit as st
 
-top = tk.Tk()
-
-top.mainloop()
+uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+for uploaded_file in uploaded_files:
+    bytes_data = uploaded_file.read()
+    st.write("filename:", uploaded_file.name)
+    st.write(bytes_data)
